ebeb6fec2a28ca2f4bacabea18c351a7aaa1be7f
Aquiles Trindade <trindadedev13@gmail.com>
2025-01-06 23:14:15 -0300

    Revert "chore(deps): update dependency com.google.android.material:material t…"
    This reverts commit 707bacb3f08b76ecfa9c09b52aaa5d812f813a6f.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,7 @@ android {
 
 dependencies {
   implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-  implementation("com.google.android.material:material:1.12.0")
+  implementation("com.google.android.material:material:1.9.0")
   implementation("androidx.appcompat:appcompat:1.6.1")
   implementation("com.github.bumptech.glide:glide:4.16.0")
   implementation("com.google.code.gson:gson:2.11.0")
