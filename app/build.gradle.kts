2e1253f69c25658fcbdc0e1ce28bd9584f8df19e
Mustiy0 <87241434+Mustiy0@users.noreply.github.com>
2025-01-09 01:00:55 +0300

    updating libs and gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,9 +54,9 @@ android {
 }
 
 dependencies {
-  implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-  implementation("com.google.android.material:material:1.9.0")
-  implementation("androidx.appcompat:appcompat:1.6.1")
+  implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+  implementation("com.google.android.material:material:1.12.0")
+  implementation("androidx.appcompat:appcompat:1.7.0")
   implementation("com.github.bumptech.glide:glide:4.16.0")
   implementation("com.google.code.gson:gson:2.11.0")
 }
